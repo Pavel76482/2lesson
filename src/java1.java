22700d3f753e1a0f9cbc3701fe68aa8bdf8486df
chydomonah@gmail.com <itmir.pavel@gmail.com>
2022-06-09 07:39:37 +0300

    123

--- src/java1.java
+++ src/java1.java
@@ -1,2 +1,145 @@
-package PACKAGE_NAME;public class java1 {
+import java.util.Scanner;
+
+public class java1 {
+    public static void main(String[] args) {
+        /*
+        int monthNumber = sc.nextInt();
+
+        if (monthNumber == 1) {
+            System.out.print("January");
+        } else if (monthNumber == 2) {
+                System.out.print("February");
+            }  else if (monthNumber == 3) {
+                    System.out.print("March");
+                }  else if (monthNumber == 4) {
+                        System.out.print("April");
+                    }  else if (monthNumber == 5) {
+                            System.out.print("May");
+                        }  else if (monthNumber == 6) {
+                            System.out.print("June");
+                            }  else if (monthNumber == 7) {
+                                    System.out.print("July");
+                                }  else if (monthNumber == 8) {
+                                        System.out.print("August");
+                                    }  else if (monthNumber == 9) {
+                                            System.out.print("September");
+                                        }  else if (monthNumber == 10) {
+                                                System.out.print("October");
+                                            }  else if (monthNumber == 11) {
+                                                    System.out.print("November");
+                                                }  else if (monthNumber == 12) {
+                                                        System.out.print("December");
+                                                    } else {
+                                                } */
+
+
+       /* 3)for (int i = 10; i < 21; i++) {
+            System.out.print(i * i);
+        } */
+        /*for (int i = 1; i < 11; i++) {
+            for (int p = 1; p < 11; i++) {
+                System.out.print(i);
+                System.out.print(p);
+            }
+        } */
+        /*Scanner sc = new Scanner(System.in);
+        int monthNumber = sc.nextInt();
+        switch (monthNumber) {
+            case 1:
+                System.out.print("January");
+                break;
+            case 2:
+                System.out.print("February");
+                break;
+            case 3:
+                System.out.print("March");
+                break;
+            case 4:
+                System.out.print("April");
+                break;
+            case 5:
+                System.out.print("May");
+                break;
+            case 6:
+                System.out.print("June");
+                break;
+            case 7:
+                System.out.print("July");
+                break;
+            case 8:
+                System.out.print("August");
+                break;
+            case 9:
+                System.out.print("September");
+                break;
+            case 10:
+                System.out.print("October");
+                break;
+            case 11:
+                System.out.print("November");
+                break;
+            case 12:
+                System.out.print("December");
+                break;
+        } */
+        /* Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+
+        if (value % 2 == 0) {
+            System.out.print("Четное число");
+        } else {
+            System.out.print("Нечетное число");
+
+        } */
+        /*Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        if (t > -5) {
+            System.out.print("Тепло");
+        } else if (-5 >= t && t > -20) {
+            System.out.print("Нормально");
+        } else if (-20 >= t) {
+            System.out.print("Холодно");
+        } else {
+
+        } */
+
+       /* Scanner sc = new Scanner(System.in);
+        int color = sc.nextInt();
+        switch (color) {
+            case 1 :
+                System.out.print("Red");
+                break;
+            case 2 :
+                System.out.print("Orange");
+                break;
+            case 3 :
+                System.out.print("Yellow");
+                break;
+            case 4 :
+                System.out.print("Green");
+                break;
+            case 5 :
+                System.out.print("Blue");
+                break;
+            case 6 :
+                System.out.print("Indigo");
+                break;
+            case 7 :
+                System.out.print("Violet");
+                break;
+        } */
+
+        /* 2)циклы  for (int i = 0; i < 100; i += 2) {
+            System.out.print(i);
+        } */
+
+/*
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i);
+        } */
+
+
+
+
+    }
 }
